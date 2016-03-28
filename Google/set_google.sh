4a37d0f4739a2ce6a26c9212a6ee749209a08f87
darkcrash21 <mike.han.83@gmail.com>
2016-03-28 02:11:20 -0700

    modified:   Google/set_google.sh

--- Google/set_google.sh
+++ Google/set_google.sh
@@ -11,3 +11,11 @@ sudo apt-get install libglib2.0-bin
 
 echo Compiling Schemas
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas
+
+echo Downloading Chromium
+
+wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser-l10n_48.0.2564.82-0ubuntu0.15.04.1.1193_all.deb
+wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
+wget https://dl.dropboxusercontent.com/u/87113035/chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
+sudo dpkg -i chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
+sudo dpkg -i chromium-browser-l10n_48.0.2564.82-0ubuntu0.15.04.1.1193_all.deb chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
