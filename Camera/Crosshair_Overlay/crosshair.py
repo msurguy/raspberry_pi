995a451b3e45076f1a1ec485d06dbad99ef46026
darkcrash21 <mike.han.83@gmail.com>
2016-03-28 02:05:44 -0700

    modified:   Camera/Crosshair_Overlay/crosshair.py 	modified:   README.md

--- Camera/Crosshair_Overlay/crosshair.py
+++ Camera/Crosshair_Overlay/crosshair.py
@@ -2,11 +2,18 @@ import picamera
 import time
 import numpy
 
+# Defines for script
+DEF_PYTHON_COMP = 3.2
+
+# Cross Hair Array
 crosshairArray = numpy.zeros((480, 640, 3), dtype=numpy.uint8)
 crosshairArray[240, :, :] = 0xff
 crosshairArray[:, 320, :] = 0xff
 
+# Blank Screen
 blankArray = numpy.zeros((480, 640, 3), dtype=numpy.uint8)
+
+overlayOn = True
 
 with picamera.PiCamera() as camera:
    camera.resolution = (640, 480)
@@ -14,10 +21,29 @@ with picamera.PiCamera() as camera:
    camera.led = False
    camera.start_preview()
 
-   overlay = camera.add_overlay(numpy.getbuffer(crosshairArray), layer = 3, alpha = 64)
+   if DEF_PYTHON_COMP == 2.7:
+      overlay = camera.add_overlay(numpy.getbuffer(crosshairArray), layer = 3, alpha = 64)
+   else:
+      overlay = camera.add_overlay(memoryview(crosshairArray), layer = 3, alpha = 20) 
 
+   time.sleep(1)
    try:
-       while True:
-           time.sleep(1)
+      while True:
+         # Turn overlay off if it's on
+         if overlayOn:
+            if DEF_PYTHON_COMP == 2.7:
+               overlay.update(numpy.getbuffer(blankArray))
+            else:
+               overlay.update(memoryview(blankArray))
+         else:
+            if DEF_PYTHON_COMP == 2.7:
+               overlay.update(numpy.getbuffer(crosshairArray))
+            else:
+               overlay.update(memoryview(crosshairArray))
+
+         overlayOn = not overlayOn
+
+         print('Overlay is: ', overlayOn)
+         time.sleep(1)
    finally:
        camera.remove_overlay(overlay)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # raspberry_pi
 2016-03-28 - Added Google schema for Epiphany Browser
+             Added Camera Directory
+             Added Overlay using Python 3.2
