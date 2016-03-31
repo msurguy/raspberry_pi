c7660744a8cabbc82c328dbf69b2118443e19e8d
darkcrash21 <mike.han.83@gmail.com>
2016-03-30 23:27:59 -0700

    new file:   C++/main.cpp 	new file:   Camera/Crosshair_Overlay/TextOverlay.py

---
+++ Camera/Crosshair_Overlay/TextOverlay.py
@@ -0,0 +1,14 @@
+
+# Text Overlay Class
+class TextOverlayClass:
+   height = 0
+   width = 0
+   def __init__(self, h, w):
+      TextOverlayClass.height = h
+      TextOverlayClass.width = w
+
+   def GetHeight():
+      return TextOverlayClass.height
+
+   def GetWidth():
+      return TextOverlayClass.width
--- Camera/Crosshair_Overlay/crosshair.py
+++ Camera/Crosshair_Overlay/crosshair.py
@@ -1,49 +1,41 @@
 import picamera
 import time
 import numpy
+import TextOverlay
 
-# Defines for script
-DEF_PYTHON_COMP = 3.2
+
+# Video Resolution
+txtoverlay = TextOverlay.TextOverlayClass(480, 640)
 
 # Cross Hair Array
-crosshairArray = numpy.zeros((480, 640, 3), dtype=numpy.uint8)
+crosshairArray = numpy.zeros((txtoverlay.GetHeight(), txtoverlay.GetWidth(), 3), dtype=numpy.uint8)
 crosshairArray[240, :, :] = 0xff
 crosshairArray[:, 320, :] = 0xff
 
 # Blank Screen
-blankArray = numpy.zeros((480, 640, 3), dtype=numpy.uint8)
+blankArray = numpy.zeros((VIDEO_HEIGHT, VIDEO_WIDTH, 3), dtype=numpy.uint8)
 
 overlayOn = True
 
 with picamera.PiCamera() as camera:
-   camera.resolution = (640, 480)
+   camera.resolution = (VIDEO_WIDTH, VIDEO_HEIGHT)
    camera.framerate = 60
    camera.led = False
    camera.start_preview()
 
-   if DEF_PYTHON_COMP == 2.7:
-      overlay = camera.add_overlay(numpy.getbuffer(crosshairArray), layer = 3, alpha = 64)
-   else:
-      overlay = camera.add_overlay(memoryview(crosshairArray), layer = 3, alpha = 20) 
+   overlay = camera.add_overlay(memoryview(crosshairArray), layer = 3, alpha = 20) 
 
    time.sleep(1)
    try:
       while True:
          # Turn overlay off if it's on
          if overlayOn:
-            if DEF_PYTHON_COMP == 2.7:
-               overlay.update(numpy.getbuffer(blankArray))
-            else:
-               overlay.update(memoryview(blankArray))
+            overlay.update(memoryview(blankArray))
          else:
-            if DEF_PYTHON_COMP == 2.7:
-               overlay.update(numpy.getbuffer(crosshairArray))
-            else:
-               overlay.update(memoryview(crosshairArray))
+            overlay.update(memoryview(crosshairArray))
 
          overlayOn = not overlayOn
 
-         print('Overlay is: ', overlayOn)
          time.sleep(1)
    finally:
        camera.remove_overlay(overlay)
