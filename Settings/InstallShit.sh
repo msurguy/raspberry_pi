eb1458c6229bae6e0bd5b753f254a84039eefb68
darkcrash21 <mike.han.83@gmail.com>
2016-04-01 01:42:33 -0700

    modified:   InstallShit.sh 	deleted:    ../C++/QT_Projects/TestApp/Test/CMakeLists.txt 	deleted:    ../C++/QT_Projects/TestApp/Test/main.cpp 	deleted:    ../C++/QT_Projects/TestApp/Test/untitled/CMakeLists.txt 	deleted:    ../C++/QT_Projects/TestApp/Test/untitled/main.cpp

--- Settings/InstallShit.sh
+++ Settings/InstallShit.sh
@@ -4,4 +4,11 @@ echo Update the Firmware
 sudo rpi-update
 
 echo Installing QT
+sudo apt-get install qt4-dev-tools
 sudo apt-get install qtcreator
+sudo apt-get install gcc
+sudo apt-get install cmake
+sudo apt-get install git-core
+
+echo Installing Pinta paint app
+sudo apt-get install pinta
