805dfd1240af35272dbab32e9b7e0d19c7f03cb0
darkcrash21 <mike.han.83@gmail.com>
2016-06-29 23:06:27 -0700

    changed Settings

--- Settings/InstallShit.sh
+++ Settings/InstallShit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo Updating the distro
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo apt-get clean
+
 echo Update the Firmware
 sudo rpi-update
 
@@ -12,3 +18,23 @@ sudo apt-get install git-core
 
 echo Installing Pinta paint app
 sudo apt-get install pinta
+
+echo Backing up Epiphany Schema
+sudo cp /usr/share/glib-2.0/schemas/org.gnome.epiphany.gschema.xml /usr/share/glib-2.0/schemas/org.gnome.epiphany.gschema.xml_backup
+
+echo Copying modified Epiphany Schema
+sudo cp ./org.gnome.epiphany.gschema.xml /usr/share/glib-2.0/schemas/org.gnome.epiphany.gschema.xml
+
+echo Installing libglib2.0-bin
+sudo apt-get install libglib2.0-bin
+
+echo Compiling Schemas
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas
+
+echo Downloading Chromium
+
+wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser-l10n_48.0.2564.82-0ubuntu0.15.04.1.1193_all.deb
+wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
+wget https://dl.dropboxusercontent.com/u/87113035/chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
+sudo dpkg -i chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
+sudo dpkg -i chromium-browser-l10n_48.0.2564.82-0ubuntu0.15.04.1.1193_all.deb chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
